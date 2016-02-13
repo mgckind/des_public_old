5923c6f55d2ce837fe4a8794ee7b7b1e2c4f3490
Matias Carrasco Kind <mcarras2@illinois.edu>
2016-02-13 14:51:35 -0600

    changed to port 8888

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import Settings
 import jira_ticket
 from tornado.options import define, options
 
-define("port", default=5000, help="run on the given port", type=int)
+define("port", default=8888, help="run on the given port", type=int)
 
 class MainHandler(tornado.web.RequestHandler):
     """
